e9f4e22946644ae9e4e0df0ecdd1bc0553aa8e8c
w3i3538 <wei5566@gmail.com>
2023-06-13 20:12:19 +0800

    refactor: modify mongoose config, fix: routes

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
@@ -7,34 +6,19 @@ const routes = require('./routes')
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
-
 const app = express()
 const port = 3000
-
-//設定連線到DB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-//取得DB連線狀態
-const db = mongoose.connection
-db.on('error', () => {
-    console.log('mongodb error!')
-})
-db.once('open', () => {
-    console.log('mongodb connected!')
-})
-
+require('./config/mongoose')
 //引擎設置
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
 //靜態檔案
 app.use(express.static('public'))
-
 //每一個req都會經過bodyParser使用urlencoded解析
 app.use(bodyParser.urlencoded({ extended: true }))
-
+//方法覆蓋
 app.use(methodOverride('_method'))
-
+//路由
 app.use(routes)
 
 //確認伺服器是否開啟
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,19 +1,11 @@
-const mongoose = require('mongoose')
 const Restaurant = require('../restaurant') 
 const restaurantList = require("../../restaurant.json").results
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-    console.log('mongodb error!')
-})
+const db = require('../../config/mongoose')
 
 db.once("open", () => {
-    console.log("running restaurantSeeder script...")
-
     Restaurant.create(restaurantList)
         .then(() => {
             console.log("restaurantSeeder done!")
