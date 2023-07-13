6306bbf6db7131243dfd67b01e477795f2a7d22c
w3i3538 <wei5566@gmail.com>
2023-07-13 19:34:57 +0800

    feat: add user authentication in some routers

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,13 @@ const home = require('./modules/home')
 const search = require('./modules/search')
 const restaurants = require('./modules/restaurants')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
+
+const { authenticator } = require('../middleware/auth')
 
 router.use('/users', users)
-router.use('/', home)
-router.use('/search', search)
-router.use('/restaurants', restaurants)
-
-
+router.use('/search', authenticator, search)
+router.use('/restaurants', authenticator, restaurants)
+router.use('/', authenticator, home)
 
 module.exports = router
