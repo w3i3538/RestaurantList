a95c88280fc77f5ff6b40e04db716834fe944b7a
w3i3538 <wei5566@gmail.com>
2023-07-13 18:26:18 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const router = express.Router()
+
+
 router.get('/login', (req, res) => {
     res.render('login')
 })
+
+router.post('/login', (req, res) => {
+})
+
+router.get('/register', (req, res) => {
+    res.render('register')
+})
+
+router.post('/register', (req, res) => {
+})
+
 module.exports = router
